960d5a14406a3ec578da727bb3f3946dcaf5ea8e
Rajat0063 <rajatyadav5641@gmail.com>
2025-01-08 00:39:38 +0530

    Completed

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-let tasks = [];
 
-const addTask = () => {
-    const taskInput = document.getElementById('taskInput');
-    const text = taskInput.value.trim();
+const inputbox = document.getElementById('input-box');
+const listcontainer = document.getElementById('list-container');
 
-    if(text) {
-        tasks.push({ text: text, completed: false});
-        taskInput.value = "";
-        updateTasksList();
+function addTask() {
+    if (inputbox.value === '') {
+        alert("You must write something");
     }
-};
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+    savedata();
+}
 
-const updateTasksList = () => {
-    const taskList = document.getElementById("task-list");
-    taskList.innerHTML = "";
+listcontainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        savedata();
+    }
+}, false);
 
-    tasks.forEach((task, index) => {
-        const listItem = document.createElement("li");
 
-        listItem.innerHTML = `
-        <div class="taskItem">
-            <div class="task ${task.completed ? 'completed' : ''}">
-                <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""} />
-                <p>${task.text}</p>
-            </div>
-            <div class="icons">
-                <img src="./assets/edit.png" onclick="editTask(${index})" />
-                <img src="./assets/bin.png" onclick="deleteTask(${index})" />
-            </div>
-        </div>
-        `;
-        listItem.addEventListener("change", () => toggleTaskComplete(index));
-        taskList.append(listItem);
-    });
-};
+function savedata() {
+    localStorage.setItem("data", listcontainer.innerHTML);
+}
 
-document.getElementById("submit").addEventListener("click", function (e) {
-    e.preventDefault();
+function showlist() {
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
 
-    addTask();
-});
+showlist();
